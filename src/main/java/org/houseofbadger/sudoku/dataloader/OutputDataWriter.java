fe5f3a535bfd22bd66bbcea205e9f57071464273
alexander gorlov <aag031.maillist@gmail.com>
2021-01-14 21:50:42 +0300

    add data writer

---
+++ src/main/java/org/houseofbadger/sudoku/dataloader/OutputDataWriter.java
@@ -0,0 +1,36 @@
+package org.houseofbadger.sudoku.dataloader;
+
+import org.houseofbadger.sudoku.model.AtomicCell;
+import org.houseofbadger.sudoku.model.MainGameContainer;
+import org.houseofbadger.sudoku.model.VectorContainer;
+import org.houseofbadger.sudoku.solutions.SolutionsSeeker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutputDataWriter {
+    private static final Logger logger= LoggerFactory.getLogger(OutputDataWriter.class);
+    List<String> outputContent = new ArrayList<>();
+    public void saveMainGameContainer(MainGameContainer mainGameContainer, String outputFileName) {
+        int lineSize = mainGameContainer.getLineSize();
+        StringBuffer sb = new StringBuffer();
+        List<VectorContainer> vectorRowList = mainGameContainer.getVectorContainerRowList();
+        for (VectorContainer vectorContainer : vectorRowList) {
+            for (int col = 0; col < lineSize; col++) {
+                AtomicCell cell = vectorContainer.getAtomicCell(col);
+                sb.append(cell.getValue() + ";");
+            }
+            outputContent.add(sb.toString());
+            try {
+                Files.write(Paths.get(outputFileName), outputContent);
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+            }
+        }
+    }
+}
